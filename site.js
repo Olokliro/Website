58c42e4db1a17dff891009fb8a783dc0b87bd2b1
davepaul <dave@davepaul.ca>
2016-10-11 23:47:28 -0400

    scroll support firefox ie

--- site.js
+++ site.js
@@ -1,9 +1,9 @@
-var body = document.getElementsByTagName("body")[0];
+//var body = document.getElementById("bodyElement");//document.getElementsByTagName("body")[0];
 var navigation = document.getElementById("navigation");
 
 
 window.addEventListener("scroll", function (evt) {
-    if (body.scrollTop > navigation.getBoundingClientRect().bottom) {
+    if (window.pageYOffset > navigation.getBoundingClientRect().bottom || document.documentElement.scrollTop > navigation.getBoundingClientRect().bottom || document.body.scrollTop > navigation.getBoundingClientRect().bottom || 0 > navigation.getBoundingClientRect().bottom) {
         navigation.className = "navbar navfixed";
     } else {
         navigation.className = "navbar navstatic";
@@ -21,6 +21,7 @@ function loadContent(source) {
     }
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+            console.log(xmlhttp.responseText);
             document.getElementById("content").innerHTML = xmlhttp.responseText;
         }
     }
